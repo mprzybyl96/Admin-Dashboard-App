2da6f278a997594268984622d88ad797a13aeb6c
maciejprzybyl-sa <maciej.przybyl@shop-apotheke.com>
2023-02-24 14:20:52 +0100

    added mocks and modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
+export const App = () => {
+  return <div>App</div>;
+};
